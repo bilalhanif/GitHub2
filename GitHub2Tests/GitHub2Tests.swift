ddd744504189c3b2b0240b3ab13ca0962e6769e8
Waqar Kayani <waqarkayani@Waqars-MacBook-Pro.local>
2016-04-25 17:24:38 +0100

    Initial status

---
+++ GitHub2Tests/GitHub2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GitHub2Tests.swift
+//  GitHub2Tests
+//
+//  Created by Waqar Kayani on 25/04/2016.
+//  Copyright © 2016 LNL. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub2
+
+class GitHub2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
